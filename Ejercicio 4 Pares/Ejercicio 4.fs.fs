a46553c462df8dafa9ed2849fc53d5d4a5f83e76
Aliss-ux <75227574+Aliss-ux@users.noreply.github.com>
2021-09-23 13:46:32 -0400

    Update Ejercicio 4.fs.fs

--- Ejercicio 4 Pares/Ejercicio 4.fs.fs
+++ Ejercicio 4 Pares/Ejercicio 4.fs.fs
@@ -8,9 +8,11 @@ let main argv =
     let pares = [for i in lista do if i%2=0 then yield i]
     printfn "La cantidad de pares es: %d" (pares.Length)
     printfn("Pares: %A") pares
+    
+    
     let impares = [for i in lista do if i%2=1 then yield i]
     printfn "La cantidad de impares es: %d" (impares.Length)
-    printfn("Impares %A") impares
+    printfn("Impares: %A") impares
         
     Console.ReadKey() |>ignore
     0 // return an integer exit code
